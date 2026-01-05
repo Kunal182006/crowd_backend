6de8983ed447e5a837142b5c2733bf8f7f7a7f1e
Kunal <kunalpanchakshari@gmail.com>
2026-01-05 21:17:26 +0530

    Fix SSL config for Supabase connection

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = process.env.PORT || 3000;
 // Connect to Supabase Postgres using environment variable
 const pool = new Pool({
   connectionString: process.env.SUPABASE_DB_URL,
-  ssl: true
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Middleware
